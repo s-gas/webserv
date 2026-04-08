8f2693e7fb3eb072949fb2e9a6135258909dc2d0
Sergej Gavrilov <sgavrilov@kopfdreher.de>
2026-04-08 18:47:13 +0200

    test: ci tester

--- src/Log.cpp
+++ src/Log.cpp
@@ -25,6 +25,7 @@ void Log::setLogFile(const std::string &filename) {
 
 // private ---------------------------------------------------------------------
 
+
 void Log::log(logLevel level, const std::string &msg) {
   std::string label;
   std::string color;
